fb2452ebe39727d84282910efab1665e7d003259
kekonline <kekonline@gmail.com>
2023-11-25 23:17:33 +0100

    Created hompage and card component also some style and stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,89 @@
 <script lang="ts">
+	import { Button, Card } from '$components';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	//sections is an array of objects that contains the title, path and items
+	let sections: {
+		title: string;
+		path: string;
+		items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[];
+	}[] = [];
+
+	//pushing the new releases, featured playlists and categories playlists to the sections array
+	$: {
+		if (data.newReleases) {
+			sections.push({
+				title: 'New Releases',
+				path: '/sections/new-releases',
+				items: data.newReleases.albums.items
+			});
+		}
+		if (data.featuredPlaylists) {
+			sections.push({
+				title: 'Featured Playlists',
+				path: '/sections/featured-playlists',
+				items: data.featuredPlaylists.playlists.items
+			});
+		}
+		//as categories was and array of objects we need to loop through it
+		data.homeCategories.forEach((category, index) => {
+			const categoryPlaylist = data.categoriesPlaylists[index];
+			if (categoryPlaylist) {
+				sections.push({
+					title: category.name,
+					path: `/category/${category.id}`,
+					items: categoryPlaylist.playlists.items
+				});
+			}
+		});
+		if (data.userPlaylists) {
+			sections.push({
+				title: 'Your Playlists',
+				path: '/playlists',
+				items: data.userPlaylists.items
+			});
+		}
+	}
 </script>
+
+{#each sections as section}
+	<section class="content-row">
+		<div class="content-row-header">
+			<div class="right">
+				<h2 class="section-title">{section.title}</h2>
+			</div>
+			<div class="left">
+				<!-- use visual hidden to hide the text for screen readers as see all is vary vague and add section.title -->
+				<Button element="a" href={section.path} variant="outline"
+					>See All <span class="visually-hidden">{section.title}</span></Button
+				>
+			</div>
+		</div>
+		<div class="grid-items">
+			{#each section.items as item}
+				<div class="grid-item" style="background-color: black;">
+					<Card {item} />
+				</div>
+			{/each}
+		</div>
+	</section>
+{/each}
+
+<style lang="scss">
+	.content-row {
+		margin-bottom: 40px;
+		.content-row-header {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-bottom: 20px;
+			.section-title {
+				font-size: functions.toRem(22);
+				font-weight: 600;
+				margin: 0;
+			}
+		}
+	}
+</style>
